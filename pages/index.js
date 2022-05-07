a6405bf84ea86dc1d91e42d8bd44472eec08e184
Kenny <kennygbeminiyi@gmail.com>
2022-05-07 07:05:27 +0100

    prisma config

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
-export default function HomePage() {
+import prisma from '../lib/prisma';
+
+export const getStaticProps = async () => {
+  const users = await prisma.user.findMany();
+  return { props: { users }};
+}
+const createUser = async () => {
+  await prisma.user.create({
+    data: {
+      name: 'Jane Doe',
+      email: 'janed@mail.com',
+      username: 'janierocks'
+    }
+  })
+}
+
+export default function Homepage({users})
+{
+  console.log(users)
   return (
     <div>
-      <h1>Tweetler</h1>
+      {users}
+      <button onClick={createUser}>create</button>
     </div>
   )
 }
-
